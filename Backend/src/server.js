cf2f874deb959f83980b064edaa921d10bcbe5d0
Abrafayansari <ansarirafay919@gmail.com>
2026-01-23 14:58:44 -0800

    test

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,10 +3,13 @@ import app from "./app.js";
 import { connectdb } from "./config/db.js";
 
 dotenv.config();
-connectdb();
-const PORT = process.env.PORT || 5000;
+const startServer = async () => {
+  await connectdb();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+startServer();
+
 
